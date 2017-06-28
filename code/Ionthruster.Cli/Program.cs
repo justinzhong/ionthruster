945830b2169a4efa23485da5d40b24f7246076ba
Justin Zhong <email@justinzhong.com>
2017-06-28 12:13:25 +1000

    Added CLI project

---
+++ code/Ionthruster.Cli/Program.cs
@@ -0,0 +1,27 @@
+﻿using Ionthruster.Config;
+using Ionthruster.Middleware;
+using Ionthruster.Tasks;
+using System;
+using System.Threading.Tasks;
+
+namespace Ionthruster.Cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task.Run(() => Thruster.StartAsync(Run)).Wait();
+
+            Console.ReadKey();
+        }
+
+        private static async Task Run(IPipelineScope scope)
+        {
+            var buildConfig = new BuildConfig();
+
+            // User defined pipeline starts here
+            await scope.Start<GitVersionTask>(buildConfig.ProjectPath)
+                .Flush();
+        }
+    }
+}
